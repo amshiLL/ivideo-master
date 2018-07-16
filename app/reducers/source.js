c0dc2c764449ba0ba4c1599bf0334fe57ee5dd7a
anshiLL <33309071+anshiLL@users.noreply.github.com>
2018-07-16 11:39:32 +0800

    Revert "Initial commit"
    This reverts commit 0379a53187f107d0b4cabe9f952f633cd13a14ef.

--- app/reducers/source.js
+++
@@ -1,8 +0,0 @@
-export default function source(state = null, action) {
-  switch(action.type) {
-    case 'GETALLVIDEOSOURCE':
-      return action.payload;
-    default:
-      return state;
-  }
-}
